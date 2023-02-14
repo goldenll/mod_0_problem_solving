dcba3adb2391a54b8bf42e09dc75f245c3d066ac
Lauren Golden <llgolden@gmail.com>
2023-02-14 11:25:33 -0700

    Push challenge1

---
+++ challenge1.rb
@@ -0,0 +1,4 @@
+# Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
+
+karen = ["i WaNt To SpEaK tO yOuR mAnAgEr"] 
+p karen.map(&:downcase)
