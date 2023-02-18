f8f2644f49c60d509f2dd0e01eb71c1b6e06b701
Lauren Golden <llgolden@gmail.com>
2023-02-17 20:40:39 -0700

    Update challenge 1

--- challenge1.rb
+++ challenge1.rb
@@ -5,5 +5,10 @@
 #  -create array
 #  -google Ruby method for how to change all letters in an array to lowercase 
 
-karen = ["i WaNt To SpEaK tO yOuR mAnAgEr"] 
-p karen.map(&:downcase)
+karens = ["i WaNt " "To SpEaK " "tO yOuR mAnAgEr"] 
+
+# p karen.map(&:downcase)
+
+karens.each do |karen|
+      puts karen.downcase
+end
